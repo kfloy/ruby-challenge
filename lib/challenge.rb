0ae7d8aa4cc75c5b8573cfc136428893c9b01ecd
Kim Davis <k.davis@evenvision.com>
2021-06-11 22:43:43 -0700

    Completing the challenge

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -7,6 +7,7 @@ class Challenge
 
   def initialize(file_path)
     @file_path = File.expand_path(file_path)
+    parse()
   end
 
   def parse
@@ -15,5 +16,49 @@ class Challenge
     # earliest_time: the earliest time contained within the data set
     # latest_time: the latest time contained within the data set
     # peak_year: the year with the most number of timestamps contained within the data set
+
+    date_arr = []
+    year_arr = []
+
+    # Opening and running through each line of the file of dates
+    File.open(@file_path).each do |date|
+      
+      # Parsing file and converting necessary parts of the datetime strings
+      timestamp = Time.parse(date).to_i
+      timezone = Time.parse(date).strftime("%:z")
+      year = Time.parse(date).strftime("%Y")
+
+      # Storing the date, timezone offset, and timestamp to an array of hashes
+      date_arr.push(:timestamp => timestamp, :timezone => timezone)
+      year_arr.push(year)
+
+    end
+
+    # Setting date-related variables for earliest and latest dates
+    earliest_date = date_arr.min{|a,b| a[:timestamp] <=> b[:timestamp]}
+    earliest_utc = Time.at(earliest_date[:timestamp]).utc
+    earliest_tzone = Time.zone_offset(earliest_date[:timezone])
+
+    latest_date = date_arr.max{|a,b| a[:timestamp] <=> b[:timestamp]}
+    latest_utc = Time.at(latest_date[:timestamp]).utc
+    latest_tzone = Time.zone_offset(latest_date[:timezone])
+
+    # Combining date variables to form the datetime string
+    earliest_combined = Time.at(earliest_utc + earliest_tzone).strftime("%Y-%m-%dT%H:%M:%S")
+    latest_combined = Time.at(latest_utc + latest_tzone).strftime("%Y-%m-%dT%H:%M:%S")
+
+    # Combining the datetime string with the correct timezone offset
+    @earliest_time = earliest_combined + "" + earliest_date[:timezone]
+    @latest_time = latest_combined + "" + latest_date[:timezone]
+
+    # Run through year_arr array and add up how many times each year exists
+    track_year_arr = Hash.new(0)
+    year_arr.each { |year|
+      track_year_arr[year] += 1
+    }
+
+    # Find the year with the most entries
+    @peak_year = track_year_arr.max{|a,b| a[1] <=> b[1]}[0]
+
   end
 end

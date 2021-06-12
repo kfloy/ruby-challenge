762527b4fedf46fbd904a11a905154786eed94c8
Kim Davis <k.davis@evenvision.com>
2021-06-12 00:27:20 -0700

    Update to challenge

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -23,32 +23,39 @@ class Challenge
     # latest_time: the latest time contained within the data set
     # peak_year: the year with the most number of timestamps contained within the data set
 
-    year_arr = []
+    year_arr = Hash.new(0)
 
     # Opening and running through each line of the file of dates
     File.open(@file_path).each do |date|
       
-      # Parsing file and converting necessary parts of the datetime strings
+      # Convert the necessary parts of the datetime strings to timestamp and year
       timestamp = Time.parse(date).to_i
-      year = Time.parse(date).strftime("%Y")
-      year_arr.push(year)
+      year = Time.parse(date).strftime("%Y");
 
+      # Run the earliest and latest mehtods, to track the earliest and latest years
       earliest(timestamp)
       latest(timestamp)
 
+      # Track how many times each year occurs
+      year_arr[year] += 1
+
     end
-    peak_years(year_arr)
+
+    # Find the year with the most entries in the year_arr hash
+    largest_year = year_arr.max{|a,b| a[1] <=> b[1]}[0]
+    @peak_year = largest_year.to_i
     
   end
 
+  # Find the earliest year
   def earliest (timestamp)
-    #@early_year = timestamp < @early_year ? timestamp : early_year
     if @early_year == '' || timestamp < @early_year
       @early_year = timestamp
       @earliest_time = Time.at(timestamp).utc
     end
   end
 
+  # Find the latest year
   def latest (timestamp)
     if @late_year == '' || timestamp > @late_year
       @late_year = timestamp
@@ -56,17 +63,4 @@ class Challenge
     end
   end
 
-  def peak_years (year_arr)
-    # Run through year_arr array and add up how many times each year exists
-    track_year_arr = Hash.new(0)
-    year_arr.each { |year|
-      track_year_arr[year] += 1
-    }
-    # Find the year with the most entries
-    largest_year = track_year_arr.max{|a,b| a[1] <=> b[1]}[0]
-    @peak_year = largest_year.to_i
-  end
-
 end
-
-#@challenge = Challenge.new("../data/timestamp2.txt")

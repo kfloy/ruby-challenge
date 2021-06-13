6f0550707cb3bbeae6bd2624695dd2173decfac8
Kim Davis <k.davis@evenvision.com>
2021-06-12 21:37:02 -0700

    Updating variable names and date method

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -7,8 +7,8 @@ class Challenge
 
   def initialize(file_path)
     @file_path = File.expand_path(file_path)
-    @late_year = ''
-    @early_year = ''
+    @later_date = ''
+    @earlier_date = ''
     @year_arr = Hash.new(0)
     parse()
   end
@@ -24,11 +24,11 @@ class Challenge
     File.open(@file_path).each do |date|
       
       # Convert the necessary parts of the datetime strings to timestamp and year
-      curr_date = Time.parse(date)
+      curr_date = Time.strptime(date, "%Y-%m-%dT%H:%M:%S%z")
       timestamp = curr_date.to_i
-      year = curr_date.year;
+      year = curr_date.year
 
-      # Track the earliest date, latest date, and peak year
+      # Run methods to track the earliest date, latest date, and peak year
       earliest_date(timestamp)
       latest_date(timestamp)
       track_year(year)
@@ -38,24 +38,24 @@ class Challenge
     # Find the year with the most entries in the year_arr hash
     common_year = @year_arr.max{|a,b| a[1] <=> b[1]}
 
-    # Converting and setting the peak_year, earliest_time, latest_time variables
+    # Assign the dates to their applicable variables
     @peak_year = common_year[0].to_i
-    @earliest_time = Time.at(@early_year).utc
-    @latest_time = Time.at(@late_year).utc
+    @earliest_time = Time.at(@earlier_date)
+    @latest_time = Time.at(@later_date)
     
   end
 
   # Find the earliest year by comparing the current timestamp to the previous earliest timestamp
   def earliest_date (timestamp)
-    if @early_year == '' || timestamp < @early_year
-      @early_year = timestamp
+    if @earlier_date == '' || timestamp < @earlier_date
+      @earlier_date = timestamp
     end
   end
 
   # Find the latest year by comparing the current timestamp to the previous latest timestamp
   def latest_date (timestamp)
-    if @late_year == '' || timestamp > @late_year
-      @late_year = timestamp
+    if @later_date == '' || timestamp > @later_date
+      @later_date = timestamp
     end
   end
 
